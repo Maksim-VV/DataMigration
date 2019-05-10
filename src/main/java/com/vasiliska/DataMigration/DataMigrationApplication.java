b3e77f501b07e339cfc44f6b435d3aba7df39e2e
MaximVV <laul@bk.ru>
2019-05-10 23:41:43 +0400

    pullR1

--- src/main/java/com/vasiliska/DataMigration/DataMigrationApplication.java
+++ src/main/java/com/vasiliska/DataMigration/DataMigrationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataMigrationApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(DataMigrationApplication.class, args);
+		
 	}
 }
